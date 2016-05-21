4161cdede9e8531879fd927b36583f39ebe1c61e
pythonstory <pincoin.info@gmail.com>
2016-05-22 05:24:51 +0900

    기본 모델 추가(메뉴 관계  표현 안함), theme 테스트 작성

--- project/rabiang/models.py
+++ project/rabiang/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
-# Create your models here.
+
+class Theme(models.Model):
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Layout(models.Model):
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Site(models.Model):
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    theme = models.ForeignKey(Theme, related_name='sites')
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    menu = models.ForeignKey(Menu, related_name='menuItems')
+    parent = models.ForeignKey('self')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Module(models.Model):
+    name = models.CharField(max_length=255)
+    site = models.ForeignKey(Site, related_name='modules')
+    theme = models.ForeignKey(Theme, related_name='modules')
+    layout = models.ForeignKey(Layout, related_name='modules')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    module = models.ForeignKey(Module, related_name='documents')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    document = models.ForeignKey(Document, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'commeted at {}'.format(self.created)
--- project/rabiang/tests.py
+++ project/rabiang/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from .models import Theme, Layout, Site
 
-# Create your tests here.
+
+class ThemeModelTest(TestCase):
+    def test_string_representation(self):
+        theme = Theme(name='rabiang theme')
+        self.assertEqual(str(theme), theme.name)
+
+
+    def test_theme_save_read(self):
+        theme = Theme(name='rabiang theme')
+        theme.save()
+
+        themes = Theme.objects.all()
+
+        self.assertEqual(themes[0].name, 'rabiang theme')
+        self.assertEqual(len(themes), 1)
+
+    def test_theme_delete(self):
+        theme = Theme(name='rabiang theme')
+        theme.save()
+
+        Theme.objects.get(id=1).delete()
+
+        themes = Theme.objects.all()
+
+        self.assertEqual(len(themes), 0)
+
+    def test_theme_update(self):
+        theme = Theme(name='rabiang theme')
+        theme.save()
+
+        t1 = Theme.objects.get(id=1)
+
+        self.assertEqual(t1.name, 'rabiang theme')
+
+        t1.name = 'another theme'
+        t1.save()
+
+        self.assertEqual(Theme.objects.get(id=1).name, 'another theme')
+
+
