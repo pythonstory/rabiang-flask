6fb471db0a227f38ab12076c38c3bd88a7f7a6fe
pythonstory <pincoin.info@gmail.com>
2016-05-23 03:01:24 +0900

    admin 사이트에 모델 등록

--- project/rabiang/admin.py
+++ project/rabiang/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Site, Theme, Layout, Menu, MenuItem, Module, Document, Comment
+
+admin.site.register(Site)
+admin.site.register(Theme)
+admin.site.register(Layout)
+admin.site.register(Menu)
+admin.site.register(MenuItem)
+admin.site.register(Module)
+admin.site.register(Document)
+admin.site.register(Comment)
+
--- project/rabiang/urls/board.py
+++ project/rabiang/urls/board.py
@@ -3,23 +3,23 @@ from .. import views
 
 urlpatterns = [
     # ex) /www/board/list
-    url(r'^list/$', views.board_list),
+    url(r'^list/$', views.board_list, name='board_list'),
 
     # ex) /www/board/show
-    url(r'^show/$', views.board_show),
+    url(r'^show/$', views.board_show, name='board_show'),
 
     # ex) /www/board/create
-    url(r'^create/$', views.board_create),
+    url(r'^create/$', views.board_create, name='board_create'),
 
     # ex) /www/board/edit
-    url(r'^edit/$', views.board_edit),
+    url(r'^edit/$', views.board_edit, name='board_edit'),
 
     # ex) /www/board/save
-    url(r'^save/$', views.board_save),
+    url(r'^save/$', views.board_save, name='board_save'),
 
     # ex) /www/board/update
-    url(r'^update/$', views.board_update),
+    url(r'^update/$', views.board_update, name='board_update'),
 
     # ex) /www/board/delete
-    url(r'^delete/$', views.board_delete),
+    url(r'^delete/$', views.board_delete, name='board_delete'),
 ]
