904010d95666ee74d638efd114aa6a7f5de634b5
pythonstory <pincoin.info@gmail.com>
2016-06-19 22:19:54 +0900

    config 파일 경로 로드 설정 변경, test.cfg TESTING=true 설정

--- run.py
+++ run.py
@@ -7,7 +7,9 @@ from app.blueprints.forum import forum
 from app.blueprints.main import main
 from app.blueprints.page import page
 
-config = os.path.join(os.path.dirname(__file__), (os.getenv('FLASK_CONFIG') or 'test.cfg'))
+BASE_DIR = os.path.dirname(__file__)
+
+config = '%s/%s' % (BASE_DIR, (os.getenv('FLASK_CONFIG') or 'test.cfg'))
 blueprints = [main, page, auth, forum]
 
 app = create_app(config=config, blueprints=blueprints)
