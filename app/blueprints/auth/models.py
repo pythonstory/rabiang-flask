c987accb08f4a3960a48a1bc44dd0ca672fb1ea6
pythonstory <pincoin.info@gmail.com>
2016-06-20 05:40:55 +0900

    비밀번호 암호화 저장 및 로그인 검사 처리

--- app/blueprints/auth/models.py
+++ app/blueprints/auth/models.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
 from app import login_manager
@@ -10,22 +11,28 @@ from app.utils.html import slugify
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
     username = db.Column(db.String(64))
     email = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(255))
-
+    password_hash = db.Column(db.String(255))
     slug = db.Column(db.String(100), unique=True)
-
     active = db.Column(db.Boolean, default=True)
-
     created_timestamp = db.Column(db.DateTime, default=datetime.now)
 
     def generate_slug(self):
         self.slug = slugify(self.username)
 
     def verify_password(self, password):
-        return True
+        return check_password_hash(self.password_hash, password)
+
+    @property
+    def password(self):
+        # password field is hidden even on the table.
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
     def __init__(self, *args, **kwargs):
         super(User, self).__init__(*args, **kwargs)
