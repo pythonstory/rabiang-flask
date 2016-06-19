1f2624062a0cb4730e5afbdb4042bbcdd7ed5224
pythonstory <pincoin.info@gmail.com>
2016-06-20 03:29:29 +0900

    StrongPassword 검증 추가

--- app/blueprints/auth/forms.py
+++ app/blueprints/auth/forms.py
@@ -4,38 +4,29 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email
 
+from app.utils.validators import Unique, StrongPassword
 from .models import User
 
 
 class LoginForm(Form):
-    email = StringField(lazy_gettext('Email'), validators=[DataRequired(), Email()])
-    password = PasswordField(lazy_gettext('Password'), validators=[DataRequired()])
+    email = StringField(lazy_gettext('Email'),
+                        validators=[DataRequired(), Email()])
+    password = PasswordField(lazy_gettext('Password'),
+                             validators=[DataRequired()])
     remember_me = BooleanField(lazy_gettext('Keep me logged in'), default=False)
     submit = SubmitField(lazy_gettext('Login'))
 
 
 class RegisterForm(Form):
-    email = StringField(lazy_gettext('Email'), validators=[DataRequired(), Email()])
-    password = PasswordField(lazy_gettext('Password'), validators=[DataRequired()])
-    password_repeat = PasswordField(lazy_gettext('Password'), validators=[DataRequired()])
-    username = StringField(lazy_gettext('Username'), validators=[DataRequired()])
-
-    def validate(self):
-        rv = Form.validate(self)
-
-        if not rv:
-            return False
-
-        user = User.query.filter_by(email=self.email.data).first()
-
-        if user is not None:
-            self.email.errors.append(lazy_gettext('Email already exists.'))
-            return False
-
-        user = User.query.filter_by(username=self.username.data).first()
-
-        if user is not None:
-            self.username.errors.append(lazy_gettext('Username already exists.'))
-            return False
-
-        return True
+    email = StringField(lazy_gettext('Email'),
+                        validators=[DataRequired(), Email(),
+                                    Unique(User, User.email,
+                                           message='There is already an account with that email.')])
+    password = PasswordField(lazy_gettext('Password'),
+                             validators=[DataRequired(), StrongPassword(message='Weak Password')])
+    password_repeat = PasswordField(lazy_gettext('Password'),
+                                    validators=[DataRequired()])
+    username = StringField(lazy_gettext('Username'),
+                           validators=[DataRequired(),
+                                       Unique(User, User.username,
+                                              message='There is already an account with that username.')])
---
+++ app/utils/validators.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import re
+
+from wtforms import ValidationError
+
+
+class Unique(object):
+    def __init__(self, model, field, message='This element already exists.'):
+        self.model = model
+        self.field = field
+        self.message = message
+
+    def __call__(self, form, field):
+        check = self.model.query.filter(self.field == field.data).first()
+
+        if check:
+            raise ValidationError(self.message)
+
+
+class StrongPassword(object):
+    def __init__(self, message='Weak password'):
+        self.message = message
+
+    def __call__(self, form, field):
+        check = re.match(r'((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20})', field.data)
+
+        """
+            (               # Start of group
+            (?=.*\d)		# must contain one digit from 0-9
+            (?=.*[a-z])		# must contain one lowercase characters
+            (?=.*[A-Z])		# must contain one uppercase characters
+            (?=.*\W)        # must contain one special symbols
+            .               # match anything with previous condition checking
+            {6,20}          # length at least 6 characters and maximum of 20
+            )
+        """
+
+        if check is None:
+            raise ValidationError(self.message)
