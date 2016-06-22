1ad6240e11fd329ce418e1f53dba618834e893a9
pythonstory <pincoin.info@gmail.com>
2016-06-23 02:21:43 +0900

    forms.py 검증 정규식 상수 처리

--- app/blueprints/auth/forms.py
+++ app/blueprints/auth/forms.py
@@ -7,6 +7,20 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp
 from app.utils.validators import Unique
 from .models import User
 
+"""
+    (               # Start of group
+    (?=.*\d)		# must contain one digit from 0-9
+    (?=.*[a-z])		# must contain one lowercase characters
+    (?=.*[A-Z])		# must contain one uppercase characters
+    (?=.*\W)        # must contain one special symbols
+    .               # match anything with previous condition checking
+    {6,20}          # length at least 6 characters and maximum of 20
+    )
+"""
+REGEX_STRONG_PASSWORD = '((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20})'
+
+REGEX_ALPHANUMERIC = '^[A-Za-z][A-Za-z0-9]*$'
+
 
 class LoginForm(Form):
     email = StringField(
@@ -32,31 +46,17 @@ class RegisterForm(Form):
             DataRequired(),
             Email(),
             Unique(
-                User,
-                User.email,
+                User, User.email,
                 message=lazy_gettext('There is already an account '
                                      'with that email.')
             )
         ]
     )
-    """
-        (               # Start of group
-        (?=.*\d)		# must contain one digit from 0-9
-        (?=.*[a-z])		# must contain one lowercase characters
-        (?=.*[A-Z])		# must contain one uppercase characters
-        (?=.*\W)        # must contain one special symbols
-        .               # match anything with previous condition checking
-        {6,20}          # length at least 6 characters and maximum of 20
-        )
-    """
     password = PasswordField(
         lazy_gettext('Password'),
         validators=[
             DataRequired(),
-            Regexp(
-                '((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20})', 0,
-                lazy_gettext('Weak Password')
-            )
+            Regexp(REGEX_STRONG_PASSWORD, 0, lazy_gettext('Weak Password'))
         ]
     )
     password_repeat = PasswordField(
@@ -71,11 +71,10 @@ class RegisterForm(Form):
         validators=[
             DataRequired(),
             Length(4, 64),
-            Regexp('^[A-Za-z][A-Za-z0-9]*$', 0,
+            Regexp(REGEX_ALPHANUMERIC, 0,
                    lazy_gettext('Username must have only letters or numbers')),
             Unique(
-                User,
-                User.username,
+                User, User.username,
                 message=lazy_gettext('There is already an account '
                                      'with that username.')
             )
@@ -90,18 +89,15 @@ class UnregisterForm(Form):
 class ChangePasswordForm(Form):
     old_password = PasswordField(
         lazy_gettext('Old Password'),
-        validators=[DataRequired()])
-
+        validators=[DataRequired()]
+    )
     password = PasswordField(
         lazy_gettext('New Password'),
         validators=[
             DataRequired(),
-            Regexp(
-                '((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20})', 0,
-                lazy_gettext('Weak Password'))
+            Regexp(REGEX_STRONG_PASSWORD, 0, lazy_gettext('Weak Password'))
         ]
     )
-
     password_repeat = PasswordField(
         lazy_gettext('Password Confirm'),
         validators=[
