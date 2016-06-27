4fd6d3dc4dbf98152e9679fe119b0ac53d05b53c
pythonstory <pincoin.info@gmail.com>
2016-06-28 00:22:22 +0900

    홈 화면 제목

--- app/blueprints/main/views.py
+++ app/blueprints/main/views.py
@@ -12,5 +12,9 @@ def index():
     print(format_datetime(datetime(1987, 3, 5, 17, 12)))
     print(gettext('Home'))
     current_app.logger.info('Information: 3 + 2 = %d', 5)
+
+    title = current_app.config.get('RABIANG_SITE_NAME')
+
     return render_template(
-        current_app.config.get('RABIANG_SITE_THEME') + '/main/index.html')
+        current_app.config.get('RABIANG_SITE_THEME') + '/main/index.html',
+        title=title)
