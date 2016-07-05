da13729ce33c880201be1370320de886e8317b8a
pythonstory <pincoin.info@gmail.com>
2016-07-06 00:18:00 +0900

    기본 홈 화면 테스트

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -6,22 +6,38 @@ from app.blueprints.auth import auth
 from app.blueprints.forum import forum
 from app.blueprints.main import main
 from app.blueprints.page import page
-from app.extensions import db
 
 
 class BasicsTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
     def setUp(self):
         blueprints = [main, page, auth, forum]
         app = create_app(config='../test.cfg', blueprints=blueprints)
         self.app = app.test_client()
-        db.create_all()
+        # db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        pass
+        # db.session.remove()
+        # db.drop_all()
 
     def test_app_exists(self):
         self.assertFalse(self.app is None)
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
 
 
 if __name__ == '__main__':
